c762a60c19148fac4bd24877bf234e67929a589a
Daxas <40495637+Daxas@users.noreply.github.com>
2021-12-11 20:18:49 +0300

    Add files via upload

---
+++ OtusHomework3/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Дарья Сербер on 11.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
